69bb50c2f25c5f878ced43ec4c03c196f405f7e0
dataserver <dataserver@users.noreply.github.com>
2018-06-19 15:14:47 -0300

    formatting

--- soundexbr.php
+++ soundexbr.php
@@ -8,13 +8,13 @@ http://www.unibratec.com.br/jornadacientifica/diretorio/NOVOB.pdf
 */
 function SoundexBR($word)
 {
-	$word = trim($word);
+    $word = trim($word);
 
-	// 1 - Transformar em maiúscula
-	$word = strtoupper($word);
+    // 1 - Transformar em maiúscula
+    $word = strtoupper($word);
 
-	// 2 - Eliminar todos os acentos das 'vogais'; 
-	$utf8 = [
+    // 2 - Eliminar todos os acentos das 'vogais'; 
+    $utf8 = [
         '/[ÁÀÂÃÄ]/u'    => 'A',
         '/[áàâãªä]/u'   => 'a',
         // '/Ç/'           => 'C',
@@ -33,9 +33,9 @@ function SoundexBR($word)
         '/[ýÿ]/u'       => 'y',
         "/['\"`´]/"     => '',
         '/[’‘‹›‚]/u'    => '',
-		'/[“”«»„]/u'    => '',
-		'/[\.]/u'       => '',
-		'/–/'           => '',
+        '/[“”«»„]/u'    => '',
+        '/[\.]/u'       => '',
+        '/–/'           => '',
         '/ª/'           => 'a.',
         '/º/'           => 'o.',
         '/°/'           => 'o.',
@@ -44,32 +44,32 @@ function SoundexBR($word)
         // '/[“”«»„]/u'    => '"', // Double quote
         '/ /'           => ' ', // nonbreaking space (equiv. to 0x160)
     ];
-	$word = preg_replace(array_keys($utf8), array_values($utf8), $word);
-	
-	// 3 - Substituição para evitar erros gerados na grafia das palavras
-	$tabela_sub = [
-		'/ç/' => 'S',
-		'/Ç/' => 'S',
-		'/BL|BR/' => 'B',
-		'/PH/' => 'F',
-		'/GL|GR|MG|NG|RG/' => 'G',
-		'/Y/' => 'I',
-		'/GE|GI|RJ|MJ/' => 'J',
-		'/CA|CO|CU|CK|Q/' => 'K',
-		'/N/' => 'M',
-		'/AO|AUM|GM|MD|OM|ON/' => 'M',
-		'/PR/' => 'P',
-		'/L/' => 'R',
-		'/CE|CI|CH|CS|RS|TS|X|Z/' => 'S',
-		'/TR|TL/' => 'T',
-		'/CT|RT|ST|PT/' => 'T',
-		'/\b[UW]/' => 'V',
-		'/RM/' => 'SM',
-		'/[MRS]+\b/' => '',
-		'/[AEIOUH]/' => '',
-	];
-	$word = preg_replace(array_keys($tabela_sub), array_values($tabela_sub), $word);
-	return $word;
+    $word = preg_replace(array_keys($utf8), array_values($utf8), $word);
+    
+    // 3 - Substituição para evitar erros gerados na grafia das palavras
+    $tabela_sub = [
+        '/ç/' => 'S',
+        '/Ç/' => 'S',
+        '/BL|BR/' => 'B',
+        '/PH/' => 'F',
+        '/GL|GR|MG|NG|RG/' => 'G',
+        '/Y/' => 'I',
+        '/GE|GI|RJ|MJ/' => 'J',
+        '/CA|CO|CU|CK|Q/' => 'K',
+        '/N/' => 'M',
+        '/AO|AUM|GM|MD|OM|ON/' => 'M',
+        '/PR/' => 'P',
+        '/L/' => 'R',
+        '/CE|CI|CH|CS|RS|TS|X|Z/' => 'S',
+        '/TR|TL/' => 'T',
+        '/CT|RT|ST|PT/' => 'T',
+        '/\b[UW]/' => 'V',
+        '/RM/' => 'SM',
+        '/[MRS]+\b/' => '',
+        '/[AEIOUH]/' => '',
+    ];
+    $word = preg_replace(array_keys($tabela_sub), array_values($tabela_sub), $word);
+    return $word;
 }
 ?>
 <html>
@@ -81,29 +81,29 @@ function SoundexBR($word)
 <pre>
 <?php
 if (isset($_GET['text'])) {
-	$text = $_GET['text'];
-		
-	echo "Origianal Texto: ". $text ."<br>";
-	echo "SoundexBR: ";
-	$arr = explode(' ', trim($text));
-	for($i=0, $y=sizeof($arr); $i < $y; $i++) {
-		if (strlen($arr[$i]) > 3) {
-			echo soundexbr($arr[$i]).' ';
-		}
-	}
+    $text = $_GET['text'];
+        
+    echo "Origianal Texto: ". $text ."<br>";
+    echo "SoundexBR: ";
+    $arr = explode(' ', trim($text));
+    for($i=0, $y=sizeof($arr); $i < $y; $i++) {
+        if (strlen($arr[$i]) > 3) {
+            echo soundexbr($arr[$i]).' ';
+        }
+    }
 }
 ?>
 </pre>
 <div align="center">
-	<form action="soundexbr.php" method="get">
-	<fieldset>
-		<legend>Fonetica</legend>	
-		<input name="text" type="text" autofocus>
-		<br><br>
-		<input name="" type="submit" value="Enviar">
-		<input type="reset" value="Limpar">
-	</form>
-	</fieldset>	
+    <form action="soundexbr.php" method="get">
+    <fieldset>
+        <legend>Fonetica</legend>   
+        <input name="text" type="text" autofocus>
+        <br><br>
+        <input name="" type="submit" value="Enviar">
+        <input type="reset" value="Limpar">
+    </form>
+    </fieldset> 
 </div>
 </body>
 </html>
@@ -118,14 +118,14 @@ http://www.unibratec.com.br/jornadacientifica/diretorio/NOVOB.pdf
 */
 function SoundexBR($word)
 {
-	$word = trim($word);
+    $word = trim($word);
 
-	// 1 - Transformar em maiúscula
-	$word = strtoupper($word);
-	// echo $word;
+    // 1 - Transformar em maiúscula
+    $word = strtoupper($word);
+    // echo $word;
 
-	// 2 - Eliminar todos os acentos das 'vogais'; 
-	$utf8 = [
+    // 2 - Eliminar todos os acentos das 'vogais'; 
+    $utf8 = [
         '/[ÁÀÂÃÄ]/u'    => 'A',
         '/[áàâãªä]/u'   => 'a',
         // '/Ç/'           => 'C',
@@ -144,9 +144,9 @@ function SoundexBR($word)
         // '/[ýÿ]/u'       => 'y',
         "/['\"`´]/"     => '',
         '/[’‘‹›‚]/u'    => '',
-		'/[“”«»„]/u'    => '',
-		'/[\.]/u'       => '',
-		'/–/'           => '',
+        '/[“”«»„]/u'    => '',
+        '/[\.]/u'       => '',
+        '/–/'           => '',
         '/ª/'           => 'a.',
         '/º/'           => 'o.',
         '/°/'           => 'o.',
@@ -155,32 +155,32 @@ function SoundexBR($word)
         // '/[“”«»„]/u'    => '"', // Double quote
         '/ /'           => ' ', // nonbreaking space (equiv. to 0x160)
     ];
-	$word = preg_replace(array_keys($utf8), array_values($utf8), $word);
-	
-	// 3 - Substituição para evitar erros gerados na grafia das palavras
-	$tabela_sub = [
-		'/ç/' => 'S',
-		'/Ç/' => 'S',
-		'/BL|BR/' => 'B',
-		'/PH/' => 'F',
-		'/GL|GR|MG|NG|RG/' => 'G',
-		'/Y/' => 'I',
-		'/GE|GI|RJ|MJ/' => 'J',
-		'/CA|CO|CU|CK|Q/' => 'K',
-		'/N/' => 'M',
-		'/AO|AUM|GM|MD|OM|ON/' => 'M',
-		'/PR/' => 'P',
-		'/L/' => 'R',
-		'/CE|CI|CH|CS|RS|TS|X|Z/' => 'S',
-		'/TR|TL/' => 'T',
-		'/CT|RT|ST|PT/' => 'T',
-		'/\b[UW]/' => 'V',
-		'/RM/' => 'SM',
-		'/[MRS]+\b/' => '',
-		'/[AEIOUH]/' => '',
-	];
-	$word = preg_replace(array_keys($tabela_sub), array_values($tabela_sub), $word);
-	return $word;
+    $word = preg_replace(array_keys($utf8), array_values($utf8), $word);
+    
+    // 3 - Substituição para evitar erros gerados na grafia das palavras
+    $tabela_sub = [
+        '/ç/' => 'S',
+        '/Ç/' => 'S',
+        '/BL|BR/' => 'B',
+        '/PH/' => 'F',
+        '/GL|GR|MG|NG|RG/' => 'G',
+        '/Y/' => 'I',
+        '/GE|GI|RJ|MJ/' => 'J',
+        '/CA|CO|CU|CK|Q/' => 'K',
+        '/N/' => 'M',
+        '/AO|AUM|GM|MD|OM|ON/' => 'M',
+        '/PR/' => 'P',
+        '/L/' => 'R',
+        '/CE|CI|CH|CS|RS|TS|X|Z/' => 'S',
+        '/TR|TL/' => 'T',
+        '/CT|RT|ST|PT/' => 'T',
+        '/\b[UW]/' => 'V',
+        '/RM/' => 'SM',
+        '/[MRS]+\b/' => '',
+        '/[AEIOUH]/' => '',
+    ];
+    $word = preg_replace(array_keys($tabela_sub), array_values($tabela_sub), $word);
+    return $word;
 }
 ?>
 <html>
@@ -192,29 +192,29 @@ function SoundexBR($word)
 <pre>
 <?php
 if (isset($_GET['text'])) {
-	$text = $_GET['text'];
-		
-	echo "Origianal Texto: ". $text ."<br>";
-	echo "SoundexBR: ";
-	$arr = explode(' ', trim($text));
-	for($i=0, $y=sizeof($arr); $i < $y; $i++) {
-		if (strlen($arr[$i]) > 3) {
-			echo soundexbr($arr[$i]).' ';
-		}
-	}
+    $text = $_GET['text'];
+        
+    echo "Origianal Texto: ". $text ."<br>";
+    echo "SoundexBR: ";
+    $arr = explode(' ', trim($text));
+    for($i=0, $y=sizeof($arr); $i < $y; $i++) {
+        if (strlen($arr[$i]) > 3) {
+            echo soundexbr($arr[$i]).' ';
+        }
+    }
 }
 ?>
 </pre>
 <div align="center">
-	<form action="soundexbr.php" method="get">
-	<fieldset>
-		<legend>Fonetica</legend>	
-		<input name="text" type="text" autofocus>
-		<br><br>
-		<input name="" type="submit" value="Enviar">
-		<input type="reset" value="Limpar">
-	</form>
-	</fieldset>	
+    <form action="soundexbr.php" method="get">
+    <fieldset>
+        <legend>Fonetica</legend>   
+        <input name="text" type="text" autofocus>
+        <br><br>
+        <input name="" type="submit" value="Enviar">
+        <input type="reset" value="Limpar">
+    </form>
+    </fieldset> 
 </div>
 </body>
 </html>
